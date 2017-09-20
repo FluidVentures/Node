a70c2f1dc4634490dad88ce3b61812389ce70622
denhamt <denhamt@users.noreply.github.com>
2017-09-20 16:01:34 -0700

    adsf
    adf

--- nRF24L01_test_RX/nRF24L01_test_RX.ino
+++ nRF24L01_test_RX/nRF24L01_test_RX.ino
@@ -1,13 +1,42 @@
+/* NMEA RMC definition
+
+  $GPRMC,001850.325,V,3754.921,N,08002.494,W,45.8,1.77,180917,,E*49
+
+  .      1         2 3       4  5       6 7   8   9   10  11 12
+       |         | |       |  |       | |   |   |    |   | |
+  $--RMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,xxxx,x.x,a*hh
+  1) Time (UTC)
+  2) Status, V = Navigation receiver warning
+  3) Latitude
+  4) N or S
+  5) Longitude
+  6) E or W
+  7) Speed over ground, knots
+  8) Track made good, degrees true
+  9) Date, ddmmyy
+  10) Magnetic Variation, degrees
+  11) E or W
+  12) Checksum
+
+*/
+
 #include <SPI.h>
 #include <Enrf24.h>
-#include <nRF24L01.h>
-#include <string.h>
 #include "PString.h"
 
+#define DEBUG_MODE true //Enable/Disable serial debug output
+
+#define RADIO_DATA_PAYLOAD_SIZE 30
+#define HEADER_MSG_CHAR  0x01
+#define DATA_MSG_CHAR    0x02
+
 Enrf24 radio(PA0, PA1, PA2); // Set up nRF24L01 radio on SPI bus pins
+
 const uint8_t rxaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x01 };
 void dump_radio_status_to_serialport(uint8_t);
 
+//********************************************** SETUP ***********************************************
+//****************************************************************************************************
 void setup() {
   Serial.begin(9600);
 
@@ -18,96 +47,27 @@ void setup() {
   radio.begin();  // Defaults 1Mbps, channel 0, max TX power
   radio.setRXaddress((void*)rxaddr);
 
-  pinMode(33, OUTPUT); //RX LED
-  digitalWrite(33, LOW); //RX LED
+  pinMode(33, OUTPUT); //TX/RX LED
+  digitalWrite(33, LOW); //TX/RX LED
 
   radio.enableRX();  // Start listening
   delay(2500); // Delay 2.5sec to open serial monitor for viewing
   dump_radio_status_to_serialport(radio.radioState());
 }
 
-void loop() {
-  digitalWrite(33, LOW);
+//**************************************** LOOP ******************************************************
+//****************************************************************************************************
+void loop()
+{
+  char tempByte[128];
 
-  char inbuf[33];
-  char latBuffer[10];
-  char lngBuffer[10];
-  byte cs;
+  while (!radio.available(true));
 
-  PString gpsLat(latBuffer, 12);
-  PString gpsLng(lngBuffer, 12);
-
-  //dump_radio_status_to_serialport(radio.radioState());  // Should show Receive Mode
-
-  while (!radio.available(true))
-    ;
-  if (radio.read(inbuf)) {
-    digitalWrite(33, HIGH); // RX LED
-
-    Serial.println(inbuf);
-
-    //Move buffer into lat/long arrays
-    for (int i = 0; i <= 7; i++)
-    {
-      digitalWrite(33, LOW); // RX LED
-      gpsLat.print(inbuf[i]);
-      digitalWrite(33, HIGH); // RX LED
-    }
-
-    for (int i = 11; i <= 20; i++)
-    {
-      digitalWrite(33, LOW); // RX LED
-      gpsLng.print(inbuf[i]);
-      digitalWrite(33, HIGH); // RX LED
-    }
-
-
-
-    /* NMEA RMC definition
-
-      $GPRMC,001850.325,V,3754.921,N,08002.494,W,45.8,1.77,180917,,E*49
-
-      .      1         2 3       4  5       6 7   8   9   10  11 12
-           |         | |       |  |       | |   |   |    |   | |
-      $--RMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,xxxx,x.x,a*hh
-      1) Time (UTC)
-      2) Status, V = Navigation receiver warning
-      3) Latitude
-      4) N or S
-      5) Longitude
-      6) E or W
-      7) Speed over ground, knots
-      8) Track made good, degrees true
-      9) Date, ddmmyy
-      10) Magnetic Variation, degrees
-      11) E or W
-      12) Checksum
-      
-      */
-
-// Create full NMEA sentence
-    char nmeabuffer[64];
-    PString nmeaSentence(nmeabuffer, sizeof(nmeabuffer));
-
-    nmeaSentence.print("$GPRMC,");
-    nmeaSentence.print("123519,"); //  1) Time (UTC)
-    nmeaSentence.print("A,");         //  2) Status, V = Navigation receiver warning
-    nmeaSentence.print(gpsLat);       //  3) Latitude
-    nmeaSentence.print(",");          //
-    nmeaSentence.print("N,");          //  4) N or S
-    nmeaSentence.print(gpsLng);       //  5) Longitude
-    nmeaSentence.print(",");          //
-    nmeaSentence.print("W,");         //  6) E or W
-    nmeaSentence.print("0.0,");       //  7) Speed over ground, knots
-    nmeaSentence.print("0.0,");       //  8) Track made good, degrees true
-    nmeaSentence.print("180917,");    //  9) Date, ddmmyy
-    nmeaSentence.print("020.3,");     //  10) Magnetic Variation, degrees
-    nmeaSentence.print("E");          //  11) E or W
-
-    Serial.println(nmeaSentence); 
+  if (radio.read(tempByte))
+  {
+    Serial.print(tempByte);
+    digitalWrite(33, !digitalRead(33)); // Turn the LED from off to on, or on to off
   }
-
-  delay(10);
 }
 
 //**************************** dump radio status*********************************************
--- nRF24L01_test_TX/nRF24L01_test_TX.ino
+++ nRF24L01_test_TX/nRF24L01_test_TX.ino
@@ -1,27 +1,57 @@
-//$GNGLL,4942.00507,N,12308.96892,W,021007.00,A,A*6C
-//$GNRMC,021008.00,A,4942.00429,N,12308.96830,W,4.860,172.27,180917,,,A*6C
-//$GNVTG,172.27,T,,M,4.860,N,9.001,K,A*20
+/* NMEA RMC definition
+
+  $GNGLL,4942.00507,N,12308.96892,W,021007.00,A,A*6C
+  $GNRMC,021008.00,A,4942.00429,N,12308.96830,W,4.860,172.27,180917,,,A*6C
+  $GNVTG,172.27,T,,M,4.860,N,9.001,K,A*20
+
+  .      1         2 3       4  5       6 7   8   9   10  11 12
+       |         | |       |  |       | |   |   |    |   | |
+  $--RMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,xxxx,x.x,a*hh
+  1) Time (UTC)
+  2) Status, V = Navigation receiver warning
+  3) Latitude
+  4) N or S
+  5) Longitude
+  6) E or W
+  7) Speed over ground, knots
+  8) Track made good, degrees true
+  9) Date, ddmmyy
+  10) Magnetic Variation, degrees
+  11) E or W
+  12) Checksum
+
+*/
+
 
 #include <SPI.h>
 #include <Enrf24.h>
 #include <TinyGPS++.h>
-#include <nRF24L01.h>
-#include <string.h>
+
+#define DEBUG_MODE true //Enable/Disable serial debug output
+
+#define RADIO_DATA_PAYLOAD_SIZE 30
+#define HEADER_MSG_CHAR  0x01
+#define DATA_MSG_CHAR    0x02
 
 Enrf24 radio(PA0, PA1, PA2);
 TinyGPSPlus gps;
 
 const int SERIAL_STRING_SIZE = 128;
+static int serialStringIndex = 0;
 
 char serialString[SERIAL_STRING_SIZE];
 int serialStringSize = 0;
-static int serialStringIndex = 0;
+int msgsSent = 0;
 
 const uint8_t txaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x01 };
 static const uint32_t GPSBaud = 9600;
 void dump_radio_status_to_serialport(uint8_t);
 int printStatus = 0;
 
+
+
+//********************************************** SETUP ***********************************************
+//****************************************************************************************************
 void setup() {
   Serial.begin(GPSBaud);
   Serial1.begin(GPSBaud);
@@ -39,7 +69,10 @@ void setup() {
   dump_radio_status_to_serialport(radio.radioState());
 }
 
+//**************************************** LOOP ******************************************************
+//****************************************************************************************************
 void loop() {
+
 
   if (readSerialString() == 1)
   {
@@ -47,29 +80,13 @@ void loop() {
     //Serial.print(serialString);
     digitalWrite(33, !digitalRead(33)); // Turn the LED from off to on, or on to off
   }
-  
-  delay(100);
-  digitalWrite(33, !digitalRead(33)); // Turn the LED from off to on, or on to off
 
-
-  /*
-    if (printStatus == 100)
-    {
-      dump_radio_status_to_serialport(radio.radioState());  // Should report IDLE
-      printStatus = 0;
-    }
-    printStatus++;
-  */
-
+  dump_radio_status_to_serialport(radio.radioState());
 }
 
 
 //**************************************** readSerialString ******************************************
-
-#define RADIO_DATA_PAYLOAD_SIZE 30
-#define HEADER_MSG_CHAR  0x01
-#define DATA_MSG_CHAR    0x02
-
+//****************************************************************************************************
 char readSerialString()
 {
   char tempByte;
@@ -81,13 +98,16 @@ char readSerialString()
     if (tempByte == '$')
     {
       //Serial.println("Found $");
+      
       serialStringIndex = 0;
       serialString[serialStringIndex++] = tempByte;
     }
     else if (tempByte == '\n')
     {
       if (serialStringIndex > 0) {
+        
         //Serial.println("Found GPS line end");
+        
         serialString[serialStringIndex++] = tempByte;
         serialStringSize = serialStringIndex;
         return 1;
@@ -104,7 +124,8 @@ char readSerialString()
 
 
 
-//**************************************** sendStringToRadio ******************************************
+//**************************************** sendStringToRadio *****************************************
+//****************************************************************************************************
 void sendStringToRadio(void)
 {
   int msgPacketCount;
@@ -116,18 +137,36 @@ void sendStringToRadio(void)
   msgPacketCount = serialStringSize / RADIO_DATA_PAYLOAD_SIZE  + 1;
   msgChecksum = 0;  //TODO calculate this.
 
-  //send header packet
-  radio.print(HEADER_MSG_CHAR);
-  //Serial.print(HEADER_MSG_CHAR);
+
+
+  msgsSent++;
+
+  radio.print(HEADER_MSG_CHAR);   //send header packet
 
   radio.print(serialStringSize);
-  //Serial.print(serialStringSize);
 
   radio.print(msgChecksum, 2);
-  //Serial.print(msgChecksum);
 
-  Serial.println();
+
   radio.flush();  // Force transmit (don't wait for any more data)
+
+  if (DEBUG_MODE == true)
+  {
+    Serial.print("msgs sent: ");
+    Serial.print(msgsSent);
+    Serial.print(", ");
+
+    Serial.print("serialStringSize: ");
+    Serial.print(serialStringSize);
+    Serial.print(", ");
+
+    Serial.print("msgChecksum: ");
+    Serial.print(msgChecksum);
+    Serial.print(", ");
+
+    Serial.print("HEADER_MSG_CHAR: ");
+    Serial.println(HEADER_MSG_CHAR);
+  }
 
 
   for (currentPacketIndex = 0; currentPacketIndex < msgPacketCount; currentPacketIndex++)
@@ -144,35 +183,47 @@ void sendStringToRadio(void)
   }
 }
 
+//**************************************** Calc Checksum ***************************
+//****************************************************************************************************
 
 
 
-//**************************************** dump_radio_status_to_serialport ******************************************
+
+//**************************************** dump_radio_status_to_serialport ***************************
+//****************************************************************************************************
 void dump_radio_status_to_serialport(uint8_t status)
 {
-  Serial.print("Enrf24 radio transceiver status: ");
-  switch (status) {
-    case ENRF24_STATE_NOTPRESENT:
-      Serial.println("NO TRANSCEIVER PRESENT");
-      break;
+  if (DEBUG_MODE == true)
+  {
+    if (printStatus == 25)
+    {
+      Serial.print("Enrf24 radio transceiver status: ");
+      switch (status) {
+        case ENRF24_STATE_NOTPRESENT:
+          Serial.println("NO TRANSCEIVER PRESENT");
+          break;
 
-    case ENRF24_STATE_DEEPSLEEP:
-      Serial.println("DEEP SLEEP <1uA power consumption");
-      break;
+        case ENRF24_STATE_DEEPSLEEP:
+          Serial.println("DEEP SLEEP <1uA power consumption");
+          break;
 
-    case ENRF24_STATE_IDLE:
-      Serial.println("IDLE module powered up w/ oscillators running");
-      break;
+        case ENRF24_STATE_IDLE:
+          Serial.println("IDLE module powered up w/ oscillators running");
+          break;
 
-    case ENRF24_STATE_PTX:
-      Serial.println("Actively Transmitting");
-      break;
+        case ENRF24_STATE_PTX:
+          Serial.println("Actively Transmitting");
+          break;
 
-    case ENRF24_STATE_PRX:
-      Serial.println("Receive Mode");
-      break;
+        case ENRF24_STATE_PRX:
+          Serial.println("Receive Mode");
+          break;
 
-    default:
-      Serial.println("UNKNOWN STATUS CODE");
+        default:
+          Serial.println("UNKNOWN STATUS CODE");
+          printStatus = 0;
+      }
+      printStatus++;
+    }
   }
 }
